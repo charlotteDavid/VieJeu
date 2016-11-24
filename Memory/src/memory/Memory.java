d6cc400595e7f4a920d21a3221381c4b06b7bb23
admin <admin@BTSWIN7-06.561557n.ac-rennes.fr>
2016-11-24 17:48:13 +0100

    debut

---
+++ Memory/src/memory/Memory.java
@@ -0,0 +1,14 @@
+package memory;
+
+public class Memory {
+
+	public static void main(String[] args) {
+		System.out.println("coucou ");
+
+		
+		
+		
+		System.out.println(" Chez moi cela fonctionne : Charlotte");
+	}
+
+}
